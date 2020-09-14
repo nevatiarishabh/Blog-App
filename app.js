8e47cc401553bec81cfef2b9111ebb529856d3d9
Rishabh Nevatia <nevatia.rishabh@gmail.com>
2020-09-14 12:43:21 +0530

    Added procfile, gitignore and port update

--- app.js
+++ app.js
@@ -107,6 +107,11 @@ app.delete("/blogs/:id", function(req, res){
     });
 });
 
-app.listen(3000, function(){
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function(){
     console.log("Server started on 3000");
 })
